13be8ef36a763f527c16421cf052f4ebffe44ff5
Rose Schneider <rschneider@mitre.org>
2025-04-30 11:02:36 -0500

    Fix treesitter

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -135,4 +135,17 @@ return {
   {
     "towolf/vim-helm",
   },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "RRethy/nvim-treesitter-endwise",
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    build = ":TSUpdate",
+    config = function()
+      require("config.treesitter")
+    end,
+    event = "BufReadPost",
+  },
 }
