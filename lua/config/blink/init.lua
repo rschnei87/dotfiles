8e808b7e6081717f50709fba961562457f32f707
Rose Schneider <rschneider@mitre.org>
2025-05-06 11:06:06 -0500

    Fix documentation to auto show

--- lua/config/blink/init.lua
+++ lua/config/blink/init.lua
@@ -5,13 +5,13 @@ blink.setup({
     preset = "none",
     ["<S-Tab>"] = { "select_prev", "fallback" },
     ["<Tab>"] = { "select_next", "fallback" },
-    ["<C-y>"] = { "select_and_accept", "fallback" },
+    ["<C-->"] = { "select_and_accept", "fallback" },
     ["<Esc>"] = { "hide", "fallback" },
     ["<Up>"] = { "scroll_documentation_up", "fallback" },
     ["<Down>"] = { "scroll_documentation_down", "fallback" },
   },
   sources = {
-    default = { "lsp", "path", "ripgrep", "snippets" },--, "snippets", "buffer" },
+    default = { "lsp", "path", "ripgrep", "snippets" },
     providers = {
       ripgrep = {
         module = "blink-ripgrep",
@@ -19,6 +19,12 @@ blink.setup({
         async = true,
       },
     },
+  },
+  completion = {
+    documentation = {
+      auto_show = true,
+      auto_show_delay_ms = 500,
+    }
   },
   fuzzy = { implementation = "rust" },
 })
